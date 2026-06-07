a28b5cb70882a71750c55e53580024f0ed9dcb65
Ismail <Ismailchaudry123@icloud.com>
2026-06-06 21:12:28 -0500

    Changed model from linear_Reg to ForestReg

--- Main.py
+++ Main.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_classification
+import numpy as np
 
 df = pd.read_csv('data.csv')
 
@@ -38,25 +39,48 @@ def one_area(area_string):
     return area_string
 
 df['Price_Crore'] = df['Price'].apply(clean_one_price)
+df = df[df['Price_Crore'] > 0]
+df['log_price'] = np.log(df['Price_Crore'])
 
 df['Area_Kanal'] = df['Area'].apply(one_area)
 df['Area_Kanal'] = pd.to_numeric(df['Area_Kanal'], errors='coerce')
 df= df.dropna(subset=['Area_Kanal'])
 
-X = df[['Area','Baths','Beds','Location']]
-y = df['Price_Crore']
+df['Beds_numeric'] = pd.to_numeric(df['Beds'], errors='coerce')
+beds_median = df['Beds_numeric'].median()
+df['Beds_clean']= df['Beds_numeric'].fillna(beds_median)
 
-Location_mean = df['Location']['Price_Crore'].mean()
-df['Location_encoded'] = df['Location'].map(Location_mean)
-global_mean = df['Location_encoded'].mean()
-df['Location_encoded'].fillna(global_mean)
+df['Baths_numeric'] = pd.to_numeric(df['Baths'], errors='coerce')
+baths_median = df['Baths_numeric'].median()
+df['Baths_clean']= df['Baths_numeric'].fillna(baths_median)
 
-model = LinearRegression()
+global_mean = df['Price_Crore'].mean()
+Location_mean = df.groupby('Location')['Price_Crore'].mean()
+location_counts = df.groupby('Location')['Price_Crore'].count()
+df['location_count'] = df['Location'].map(location_counts)
+
+df['Location_encoded'] = np.where(df['location_count'] > 10, df['Location'].map(Location_mean), global_mean )
+
+X = df[['Area_Kanal','Baths_clean','Beds_clean','Location_encoded']]
+y = df['log_price']
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.2,random_state=42)
 
-model.fit(X_train,y_train)
-y_predict = model.predict(X_test)
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 
-print("Actual Price:", y_test.values)
-print('Predicted Price:', y_predict)
+model.fit(X_train,y_train)
+log_predict = model.predict(X_test)
+y_predict = np.exp(log_predict)
+
+print('Actual Price (in Crore):', [f'{x:.2f}' for x in np.exp(y_test[:10])],)
+print('Predicted Price (in Crore):', y_predict[:10])
+
+y_test_crore = np.exp(y_test)
+mae = mean_absolute_error(y_test_crore, y_predict)
+mse = mean_squared_error(y_test_crore, y_predict)
+
+print(f'Mean Absolute Error {mae:.2f}')
+print(f'Mean Squared Error {mse:.2f}')
+
+
+
